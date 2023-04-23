1c3aa918d85fd683055e5065742137ada6c6ce0c
prasad0275 <sawant.prasad0275@gmail.com>
2023-04-23 11:34:27 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { ProductModule } from './product/product.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:ProductModule}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    ProductModule,
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/product/product.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductComponent } from './product.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes : Routes = [
+    { path:'/prouduct', component:ProductComponent}
+]
+
+@NgModule({
+  declarations: [
+    ProductComponent
+  ],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule
+  ]
+})
+export class ProductModule { }
